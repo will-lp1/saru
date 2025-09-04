8b693375764b466377e702fd3dcdc51f0e4cd411
Tanmay Jain <tanmayjain4477@gmail.com>
2025-09-04 11:50:17 +0530

    changed type Message as UIMessage

--- apps/saru/app/api/chat/actions/chat.ts
+++ apps/saru/app/api/chat/actions/chat.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { generateText, Message } from 'ai';
+import { generateText, UIMessage } from 'ai';
 import { cookies } from 'next/headers';
 
 import {
@@ -21,7 +21,7 @@ export async function saveChatModelAsCookie(model: string) {
 export async function generateTitleFromUserMessage({
   message,
 }: {
-  message: Message;
+  message: UIMessage;
 }) {
   const { text: title } = await generateText({
     model: myProvider.languageModel('title-model'),
